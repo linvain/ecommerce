ba0a45cc10fef85da099ff51e2bbd5ca16bb52e3
linvain <linvain@protonmail.com>
2019-05-16 19:16:09 +0300

    new styles

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,12 @@ import { client } from './client'
 import { ProductCardList } from './components/ProductCardList'
 import { Header } from './components/Header'
 
-const fakeProducts = [...Array(20).keys()].map((n) => {
+const fakeProducts = [...Array(40).keys()].map((n) => {
   const m = n + 1
   return {
     id: m,
     name: `Product ${m}`,
-    img: `https://lorempixel.com/150/150?random=${m}`,
+    img: `https://dummyimage.com/150x150/fff/000&text=${m}`,
   }
 })
 
