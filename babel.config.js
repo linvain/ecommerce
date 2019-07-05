bda85c0624189dba3f14c5ecf3c1d7d08386025f
linvain <linvain@protonmail.com>
2019-07-05 19:28:58 +0300

    better scripts

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  "presets": [
-    "@babel/preset-react",
-    [
-      "@babel/preset-env",
-      {
-        "useBuiltIns": "usage",
-        "corejs": 3
-      }
-    ]
-  ],
-  "plugins": ["transform-undefined-to-void"]
+	'presets': [
+		'@babel/preset-react',
+		[
+			'@babel/preset-env',
+			{
+				'useBuiltIns': 'usage',
+				'corejs': 3
+			}
+		]
+	],
+	'plugins': ['transform-undefined-to-void']
 }
